ce01c3b1e3a136ef7b3144c8f188134cf9c27be7
Erick Espinoza <eespinozaa0489@uvirtual.ac.cr>
2024-06-12 17:51:49 -0600

    encomiendas

--- resources/views/components/page-layout.blade.php
+++ resources/views/components/page-layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('title', 'prueba')</title>
+        <title>@yield('title', 'Encomiendas')</title>
         <script src="https://cdn.tailwindcss.com"></script>
 
         @stack('css')
--- resources/views/usuarios/show.blade.php
+++ resources/views/usuarios/show.blade.php
@@ -3,6 +3,7 @@
         <div class="mb-4 p-4">
             <a href="{{ route("usuarios.index") }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Volver a usuarios</a> 
         </div>
+        
         <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div class="flex justify-end px-4 pt-4">
                 <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
